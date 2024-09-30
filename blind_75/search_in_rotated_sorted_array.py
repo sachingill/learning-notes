1b25f3e6d0f49ff452002d1e40ba3cd9a4f72860
sachingill-aq <156288524+sachingill-aq@users.noreply.github.com>
2024-09-30 22:17:05 +0530

    mit
    Find index of searched element..

--- blind_75/search_in_rotated_sorted_array.py
+++ blind_75/search_in_rotated_sorted_array.py
@@ -1,2 +1,30 @@
 def search(nums: list[int], target: int) -> int:
-    pass
+    if not nums:
+       return -1
+    if len(nums) <=1:
+      return 0 if nums[0] == target else -1
+    left = 0
+    right = len(nums) -1
+    
+    while (left <= right):
+        mid = (left + right) //2
+        if (nums[mid] == target):
+            return mid;
+       
+        if nums[left] <= nums[mid]:
+           if nums[left] <= target < nums[mid]:
+              right =  mid -1
+           else:
+              left = mid+1
+        else:
+           if nums[mid] < target <= nums[right]:
+              left = mid + 1
+           else:
+              right = mid - 1
+    return -1
+
+
+    
+    
+
+
